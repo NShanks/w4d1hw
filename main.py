7774ddc90ab23c6a7b9bc4899bdae9ad0f7dc96b
Nicolas Shanks <nicolasshanks10@gmail.com>
2022-08-23 09:59:08 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import re
+
+with open('files/regex_test.txt') as f:
+    data = f.readlines()
+
+pattern = re.compile('[A-Z][a-z]+)\s([A-Z][a-z])*?\s?([A-Z][a-z]*)')
+
+for person in data:
+    match = pattern.search(person)
+    if match:
+        if match.group(2):
+            print("\n" f"{match.group(1)}{match.group(2)} {match.group(3)}")
+        else:
+            print("\n" f"{match.group(1)}{match.group(3)}")
+
+pattern = re.compile('(\w+), ([A-Za-z]+-)*([A-Za-z]+). *\s(@[a-z]+$)')
